2c2f9294003923d174fb9f13195b2062f628d5ff
Pietro Diana <pdiana.dev@gmail.com>
2020-09-13 10:09:08 +0200

    concurrently

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav>
+        <v-toolbar app>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Dummy</span>
+                <span class="grey--text text--darken-2">App</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn to="/" text>
+                    <v-icon left>home</v-icon>
+                    Home
+                </v-btn>
+                <v-btn to="/about" text class="mr-10">
+                    <v-icon left>info</v-icon>
+                    About
+                </v-btn>
+            </v-toolbar-items>
+            <v-btn text>
+                <span class="grey--text text--darken-2">Log Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+    export default {
+        items: [
+            {title: 'Home', link: '/', icon: 'home'},
+            {title: 'About', link: '/about', icon: 'info'}
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
